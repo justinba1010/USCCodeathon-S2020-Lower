b137166127e66e5a2d013d923a507932093e7e72
Justin Baum <justin.baum@ninefx.com>
2019-12-30 22:27:00 -0500

    Add budget problem with sample inputs

--- budgets/testgen.sh
+++ budgets/testgen.sh
@@ -7,17 +7,17 @@ cp $SCRIPT_DIR/input1.txt $SCRIPT_DIR/input/input1.txt
 
 pushd $SCRIPT_DIR
 
-  for i in {2..35}
+  for i in {1..32}
   do
     python3 ./mkin.py > input/input$i.txt;
     python3 ./solutions/solution1.py < input/input$i.txt > output/output$i.txt
-    python3 ./solutions/solution2.py < input/input$i.txt > output2/output$i.txt
     echo $i
   done
 
-  for i in {0..1}
+  for i in 0 33 34 35
   do
-    python3 ./solutions/solution.py < input/input$i.txt > output/output$i.txt
+    echo $i
+    python3 ./solutions/solution1.py < input/input$i.txt > output/output$i.txt
   done
 popd
 
